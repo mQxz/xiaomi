cbc5831d2e0ac4e363d199db076865970859ebcd
mQxz <33171126+mQxz@users.noreply.github.com>
2017-11-16 14:08:50 +0800

    day04

--- mi_project/js/homepage.js
+++ mi_project/js/homepage.js
@@ -117,30 +117,43 @@ requirejs(["jquery","pb"],function($,pb){
     })
     
     //楼层标题移动
-    $(".tab-more>li").mouseenter(function(){
-    	$(this).addClass("tab-active").siblings().removeClass("tab-active");
+    for(var i = 1 ; i <= 5 ;i++){
+    	floorShow($(".floor"+ i +" .tab-more>li").eq(0));
+    }
+    $(".floor1 .tab-more>li").mouseenter(function(){
+    	floorShow($(this));
+    })
+    $(".floor2 .tab-more>li").mouseenter(function(){
+    	floorShow($(this));
+    })
+    $(".floor3 .tab-more>li").mouseenter(function(){
+    	floorShow($(this));
+    })
+    $(".floor4 .tab-more>li").mouseenter(function(){
+    	floorShow($(this));
+    })
+    $(".floor5 .tab-more>li").mouseenter(function(){
+    	floorShow($(this));
+    })
+    
+    
+    function floorShow(obj){
+    	obj.addClass("tab-active").siblings().removeClass("tab-active");
     	pb.ajax( "http://127.0.0.1/xiaomi/mi_project/json/floors.json?hh="+Math.random(),function(json){
 			var str = "";
-	    	for( var i = 0 ; i < json[$(this).data("type")].list.length ; i++ ){
-	    		var product = json[$(this).data("type")].list[i];
-				if(i == json[$(this).data("type")].list.length-2){
+	    	for( var i = 0 ; i < json[obj.data("type")].list.length ; i++ ){
+	    		var product = json[obj.data("type")].list[i];
+				if(i == 7){
 					str += `<li class="brick brick-s">
 								<a class="brick-s-img" href="javascript:;">
-									<img src="img/index/floorsele/${json[$(this).data("type")].name}/${product.src}" alt="" />
+									<img src="img/index/floorsele/${json[obj.data("type")].name}/${product.src}" alt="" />
 								</a>
 								<h3 class="floor-con-title"><a href="javascript:;">${product.name}</a></h3>
 								<p class="price">${product.price}元</p>
 							</li>`;
-	    		}else if(i == json[$(this).data("type")].list.length-1){
-					str += `<li class="brick brick-s">
-								<a class="brick-s-icon" href="javascript:;">
-									<i class="iconfont icon-yijiuhuanxin"></i>
-								</a>
-								<a class="brick-s-more" href="javascript:;">浏览更多<small>热门</small></a>
-							</li>`;
-				}else{
+	    		}else{
 					str += `<li class="brick">
-								<a class="floor-con-img" href="javascript:;"><img src="img/index/floorsele/${json[$(this).data("type")].name}/${product.src}" alt=""/></a>
+								<a class="floor-con-img" href="javascript:;"><img src="img/index/floorsele/${json[obj.data("type")].name}/${product.src}" alt=""/></a>
 								<h3 class="floor-con-title"><a href="javascript:;">${product.name}</a></h3>
 								<p class="desc">${product.desc}</p>
 								<p class="price">${product.price}元</p>
@@ -150,11 +163,23 @@ requirejs(["jquery","pb"],function($,pb){
 							</li>`;
 				}
 			}
-	    	$(".floor-con-2").html(str);
+	    	str += `<li class="brick brick-s">
+						<a class="brick-s-icon" href="javascript:;">
+							<i class="iconfont icon-yijiuhuanxin"></i>
+						</a>
+						<a class="brick-s-more" href="javascript:;">浏览更多<small>热门</small></a>
+					</li>`;
+	    	obj.parent().parent().parent().find(".floor-con-2").html(str);
 	   }.bind(this));
+    }
+    
+    
+    //headernav 子元素隐藏显示
+    $(".header-nav>li").mouseover(function(){
+    	$(this).find(".item-children").css("display","block");
+    }).mouseout(function(){
+    	$(this).find(".item-children").css("display","none");
     })
-    
-    
     
     
     
